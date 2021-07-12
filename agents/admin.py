aa28a5425a295514a18c2f96e42e9304c4a371e9
Dipu Howlader <dipuhowlader33@gmail.com>
2021-07-12 20:57:02 +0600

    removing igrored files

--- agents/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Agent
-
-class AgentAdmin(admin.ModelAdmin):
-    list_display = ('username', 'email', 'age')
-
-# Register your models here.
-admin.site.register(Agent, AgentAdmin)
--- agents/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name ='agents'
-
-urlpatterns = [
-    path('', views.agentPage.as_view(), name='agentPage'),
-    path('create/', views.createAgent.as_view(), name='createAgent'),
-]
