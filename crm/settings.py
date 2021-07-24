8b66024d3e3b2a00761a96310540113950f6e707
Dipu Howlader <dipuhowlader33@gmail.com>
2021-07-24 11:43:37 +0600

    security updated

--- crm/settings.py
+++ crm/settings.py
@@ -1,5 +1,4 @@
 from pathlib import Path
-from .secrets import KEY
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -10,14 +9,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = KEY
+SECRET_KEY = 'django-insecure-md6%2v)mnt7-sj3+h#@8zg!v$#u4=^5_8r3#2_#ew)p7a1nau3'
 
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://gentle-sea-75674.herokuapp.com/']
 
 
 # Application definition
